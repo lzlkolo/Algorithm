f4be05cbf84a594bbc6f475f12e1906b0ea77627
lzlkolo <108389569+lzlkolo@users.noreply.github.com>
2024-07-26 15:27:30 +0900

    [level 0] Title: 나이 출력, Time: 0.04 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int age) {
+        int answer = 0;
+        int year = 2022;
+        int koreaAge = 1;
+        
+        answer = year - age + koreaAge;
+            
+        return answer;
+    }
+}
